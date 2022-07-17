d3f7f07b8d5a43126f504b9ad93e5596064ac09c
OlyaNikonchuk <olya.nikonchuk02@gmail.com>
2022-07-17 15:50:36 +0300

    first attempt

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        float res;
+        if (salary>0){
+            if(salary<=10000){
+                res=(float)salary-((float)(salary*0.15));
+                System.out.println(res);
+            }
+            if(salary>10000&&salary<=20000){
+                res=(float)salary-((float)(salary*0.18));
+                System.out.println(res);
+            }
+            if(salary>20000){
+                res=(float)salary-((float)(salary*0.2));
+                System.out.println(res);
+            }
+        }else{
+            System.out.println("wrong input!");
+        }
     }
 }
